8ea0cb87ea1340f998040e70ec8f2aad1282a1d3
Aaron Barraza <aaron@mightyminnow.com>
2022-09-18 23:04:31 -0700

    Enqueue script.

--- includes/class-elementor-mm-customizations.php
+++ includes/class-elementor-mm-customizations.php
@@ -9,6 +9,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Elementor_MM_Customizations {
 
 	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_assets' ) );
+	}
+
+	public function enqueue_assets() {
+		wp_enqueue_script(
+			'elementor-mm-customizations',
+			ELEMENTOR_MM_CUSTOMIZATIONS_URL . 'assets/js/elementor-mm-customizations.js',
+			array( 'jquery' ),
+			ELEMENTOR_MM_CUSTOMIZATIONS_VERSION,
+			true
+		);
 	}
 
 }
