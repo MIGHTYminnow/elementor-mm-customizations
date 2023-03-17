fdd2bd33652eebc3cef63358776dee8d28bc4e7c
Aaron Barraza <aaron@mightyminnow.com>
2023-03-17 07:17:23 -0700

    [TAG] Version 1.0.0-beta-2

--- elementor-mm-customizations.php
+++ elementor-mm-customizations.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Elementor - MM Customizations
  * Plugin URI: https://github.com/MIGHTYminnow/elementor-mm-customizations
- * Version: 1.0.0-alpha
+ * Version: 1.0.0-beta-2
  * Author: MIGHTYminnow
  * Author URI: https://mightyminnow.com
  * Description: Adds accessibility fixes to Elementor.
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'ELEMENTOR_MM_CUSTOMIZATIONS_VERSION' ) ) {
-	define( 'ELEMENTOR_MM_CUSTOMIZATIONS_VERSION', '1.0.0-alpha' );
+	define( 'ELEMENTOR_MM_CUSTOMIZATIONS_VERSION', '1.0.0-beta-2' );
 }
 
 if ( ! defined( 'ELEMENTOR_MM_CUSTOMIZATIONS_PATH' ) ) {
